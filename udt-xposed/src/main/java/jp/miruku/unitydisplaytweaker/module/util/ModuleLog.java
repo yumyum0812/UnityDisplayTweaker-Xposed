34cf94252dc7c9718f720bf94d1da7c4b67053c5
miruku <36main369@gmail.com>
2025-12-12 01:58:19 +0900

    new logging util

---
+++ udt-xposed/src/main/java/jp/miruku/unitydisplaytweaker/module/util/ModuleLog.java
@@ -0,0 +1,34 @@
+package jp.miruku.unitydisplaytweaker.module.util;
+
+import android.util.Log;
+
+import de.robv.android.xposed.XposedBridge;
+
+public class ModuleLog {
+    private static final String TAG_LOGCAT = "UnityDisplayTweaker";
+    private static final String TAG_XPOSED = "UDT";
+
+    public static void d(String msg) {
+        Log.i(TAG_LOGCAT, msg);
+        logForXposed("D", msg);
+    }
+
+    public static void i(String msg) {
+        Log.i(TAG_LOGCAT, msg);
+        logForXposed("I", msg);
+    }
+
+    public static void e(String msg) {
+        Log.e(TAG_LOGCAT, msg);
+        logForXposed("E", msg);
+    }
+
+    public static void e(String msg, Throwable t) {
+        Log.e(TAG_LOGCAT, msg, t);
+        logForXposed("E", msg + ": " + t.getClass().getName() + ": " + t.getMessage());
+    }
+
+    private static void logForXposed(String level, String msg) {
+        XposedBridge.log("[" + TAG_XPOSED + "][" + level + "] " + msg);
+    }
+}
