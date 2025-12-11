d1511c665b6a4fd193c2845980460e26877c09b6
miruku <36main369@gmail.com>
2025-12-12 01:57:08 +0900

    updated logging and constants id

--- udt-xposed/src/main/java/jp/miruku/unitydisplaytweaker/module/XposedEntry.java
+++ udt-xposed/src/main/java/jp/miruku/unitydisplaytweaker/module/XposedEntry.java
@@ -1,37 +1,42 @@
 package jp.miruku.unitydisplaytweaker.module;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import jp.miruku.unitydisplaytweaker.module.common.Constants;
+import jp.miruku.unitydisplaytweaker.module.util.ModuleLog;
 
 public class XposedEntry implements IXposedHookLoadPackage {
     public static Config config;
-
-    public static native void startApply(float delay, boolean changeResolution, int width, int height, boolean changeMaxFps, int maxFps);
 
     private void loadPrefs() {
         var prefs = new Preferences("module_config");
         config = new Config(prefs);
     }
 
+    public static Config getConfig() {
+        return config;
+    }
+
     @Override
     public void handleLoadPackage(@NonNull XC_LoadPackage.LoadPackageParam param) {
-        if (param.packageName.equals(Constants.MANAGER_PACKAGE)) return;
+        if (param.packageName.equals(Constants.MANAGER_PACKAGE_NAME)) return;
         if (!param.isFirstApplication) return;
-        Log.d(Constants.LOG_TAG, "handleLoadPackage: " + param.packageName);
+        ModuleLog.i("handleLoadPackage: " + param.packageName);
 
         loadPrefs();
 
         try {
             XposedHelpers.findAndHookMethod("android.app.Activity", param.classLoader, "onCreate", Bundle.class, new ActivityHook());
-            Log.d(Constants.LOG_TAG, "Hooked.");
+            ModuleLog.i("Hooked.");
         } catch (RuntimeException e) {
-            Log.e(Constants.LOG_TAG, e.getClass().getName() + ": " + e.getMessage());
+            ModuleLog.e("Failed to hook", e);
             throw new RuntimeException(e);
         }
     }
+
+    public static native void startApply(float delay, boolean changeResolution, int width, int height, boolean changeMaxFps, int maxFps);
 }
