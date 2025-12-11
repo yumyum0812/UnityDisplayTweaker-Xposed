997bc5efb4ef2f65ab7b0447f0ac6cf6f58665b4
miruku <36main369@gmail.com>
2025-12-12 01:55:22 +0900

    changed logging

--- udt-xposed/src/main/cpp/udt-native/src/utils/logcat.h
+++ udt-xposed/src/main/cpp/udt-native/src/utils/logcat.h
@@ -4,26 +4,18 @@
 #include <format>
 #include <android/log.h>
 
-#define LOGGING_DISABLE_DEBUG_RELEASE 1
 
-
-namespace Logging {
+namespace Logcat {
     template<typename... Args>
     void Print(int priority, std::string_view tag, std::string_view fmt, Args&&... args) {
         std::string msg = std::vformat(fmt, std::make_format_args(args...));
         __android_log_write(priority, tag.data(), msg.c_str());
     }
 
-#if defined(NDEBUG) && LOGGING_DISABLE_DEBUG_RELEASE
-    template<typename... Args>
-    __attribute__((always_inline))
-    inline void Debug(std::string_view tag, std::string_view fmt, Args&&... args) {}
-#else
     template<typename... Args>
     void Debug(std::string_view tag, std::string_view fmt, Args&&... args) {
         Print(ANDROID_LOG_DEBUG, tag, fmt, std::forward<Args>(args)...);
     }
-#endif
 
     template<typename... Args>
     void Verbose(std::string_view tag, std::string_view fmt, Args&&... args) {
