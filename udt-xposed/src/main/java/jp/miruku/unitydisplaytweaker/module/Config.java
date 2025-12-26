d651341c92aeb848aea44426af3ba0f2855d116f
miruku <36main369@gmail.com>
2025-12-26 18:43:19 +0900

    made config singleton

--- udt-xposed/src/main/java/jp/miruku/unitydisplaytweaker/module/Config.java
+++ udt-xposed/src/main/java/jp/miruku/unitydisplaytweaker/module/Config.java
@@ -4,7 +4,7 @@ public class Config {
     public boolean showToastOnLoad;
     public float delayApply;
     public boolean changeResolution;
-    public boolean useNativeResolution;
+    public boolean useWindowResolution;
     public int customWidth;
     public int customHeight;
     public boolean useResolutionScale;
@@ -14,18 +14,26 @@ public class Config {
     public boolean useNativeRefreshRate;
     public int fpsCap;
 
-    public Config(Preferences prefs) {
-        showToastOnLoad = prefs.loadBoolean("show_toast", true);
-        delayApply = prefs.loadFloatStr("delay", 5.f);
-        changeResolution = prefs.loadBoolean("change_resolution", false);
-        useNativeResolution = prefs.loadBoolean("use_native_resolution", false);
-        customWidth = prefs.loadIntStr("custom_width", 1280);
-        customHeight = prefs.loadIntStr("custom_height", 720);
-        useResolutionScale = prefs.loadBoolean("change_resolution_scale", false);
-        widthScale = prefs.loadFloatStr("width_scale", 1.f);
-        heightScale = prefs.loadFloatStr("height_scale", 1.f);
-        changeFpsCap = prefs.loadBoolean("change_max_fps", false);
-        useNativeRefreshRate = prefs.loadBoolean("use_native_refresh_rate", false);
-        fpsCap = prefs.loadIntStr("max_fps", 60);
+    private static Config config;
+
+    public static void load() {
+        var prefs = new Preferences("module_config");
+        config = new Config();
+        config.showToastOnLoad = prefs.loadBoolean("show_toast", true);
+        config.delayApply = prefs.loadFloatStr("delay", 5.f);
+        config.changeResolution = prefs.loadBoolean("change_resolution", false);
+        config.useWindowResolution = prefs.loadBoolean("use_window_resolution", false);
+        config.customWidth = prefs.loadIntStr("custom_width", 1280);
+        config.customHeight = prefs.loadIntStr("custom_height", 720);
+        config.useResolutionScale = prefs.loadBoolean("change_resolution_scale", false);
+        config.widthScale = prefs.loadFloatStr("width_scale", 1.f);
+        config.heightScale = prefs.loadFloatStr("height_scale", 1.f);
+        config.changeFpsCap = prefs.loadBoolean("change_max_fps", false);
+        config.useNativeRefreshRate = prefs.loadBoolean("use_native_refresh_rate", false);
+        config.fpsCap = prefs.loadIntStr("max_fps", 60);
+    }
+
+    public static Config getConfig() {
+        return config;
     }
 }
