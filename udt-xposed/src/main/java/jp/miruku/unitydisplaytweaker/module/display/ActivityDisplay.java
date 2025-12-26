3b3c885889e26a918f27e0432748c3909786c9ce
miruku <36main369@gmail.com>
2025-12-26 18:39:50 +0900

    updated activity display wrapper

--- udt-xposed/src/main/java/jp/miruku/unitydisplaytweaker/module/display/ActivityDisplay.java
+++ udt-xposed/src/main/java/jp/miruku/unitydisplaytweaker/module/display/ActivityDisplay.java
@@ -1,20 +1,28 @@
 package jp.miruku.unitydisplaytweaker.module.display;
 
 import android.app.Activity;
-import android.graphics.Point;
 import android.os.Build;
 import android.view.Display;
-import android.view.Surface;
 
 import androidx.annotation.NonNull;
 
 public class ActivityDisplay {
     private final Display mDisplay;
-    private final Display.Mode mMode;
 
     public ActivityDisplay(@NonNull Activity activity) {
         mDisplay = getDisplayInternal(activity);
-        mMode = mDisplay.getMode();
+    }
+
+    public float getRefreshRate() {
+        float maxRefresh = 0f;
+
+        for (var mode : mDisplay.getSupportedModes()) {
+            if (mode.getRefreshRate() > maxRefresh) {
+                maxRefresh = mode.getRefreshRate();
+            }
+        }
+
+        return maxRefresh;
     }
 
     private static Display getDisplayInternal(@NonNull Activity activity) {
@@ -27,27 +35,5 @@ public class ActivityDisplay {
             var display = activity.getWindowManager().getDefaultDisplay();
             return display;
         }
-    }
-
-    public Point getNativeResolution() {
-        int physicalWidth = mMode.getPhysicalWidth();
-        int physicalHeight = mMode.getPhysicalHeight();
-        return new Point(physicalWidth, physicalHeight);
-    }
-
-    public Point getNativeResolutionRotated() {
-        var size = getNativeResolution();
-        int w = size.x;
-        int h = size.y;
-
-        int rotation = mDisplay.getRotation();
-        if (rotation == Surface.ROTATION_90 || rotation == Surface.ROTATION_270) {
-            return new Point(h, w);
-        }
-        return size;
-    }
-
-    public float getNativeRefreshRate() {
-        return mMode.getRefreshRate();
     }
 }
