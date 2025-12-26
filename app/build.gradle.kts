b6b894312ecaa38193115a838e74f54ca4c6928d
miruku <36main369@gmail.com>
2025-12-26 18:41:38 +0900

    updated app version 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "jp.miruku.unitydisplaytweaker"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.2.0"
 
         ndk {
             abiFilters.add("armeabi-v7a")
